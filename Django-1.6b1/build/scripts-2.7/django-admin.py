3b8d01a2a92f2a3f4ad08593447951452c289774
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-13 13:13:18 +0200

    1st commit for my 1st Django app

---
+++ Django-1.6b1/build/scripts-2.7/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/eleonore/Documents/Europython_2013/03.07/Django_intro/.virtualenvs/cheesely/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
