21bae924184856e421bee310a7ec0932cdf4fcf2
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-14 23:11:05 +0200

    templates dir, views, routes

--- Django-1.6b1/cheesely_repo/cheesely/cheesely/urls.py
+++ Django-1.6b1/cheesely_repo/cheesely/cheesely/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/',
+        TemplateView.as_view(template_name='pages/about.html'),
+        name='about'),
+    url(r'^$',
+        TemplateView.as_view(template_name='home.html'),
+        name='home'),
 )
