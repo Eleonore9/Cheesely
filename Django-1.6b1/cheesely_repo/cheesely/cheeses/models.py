2aafa101ea1ba9e206a466d192165d1d242c8f31
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-15 19:42:41 +0200

    added a Django app

---
+++ Django-1.6b1/cheesely_repo/cheesely/cheeses/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Cheese(models.Model):
+	FIRMNESS_UNSPECIFIED = 1
+	FIRMNESS_SOFT = 2
+	FIRMNESS_HARD = 3
+
+	FIRMNESS_CHOICES = (
+		(FIRMNESS_UNSPECIFIED, 'unspecified'),
+		(FIRMNESS_SOFT, 'soft'),
+		(FIRMNESS_HARD, 'hard'),
+	)
+
+	name = models.CharField("Name of Cheese", max_length=255)
+	description = models.TextField("Description", blank=True,
+				null=True)
+	firmness = models.IntegerField("Firmness", max_length=20,
+				choices=FIRMNESS_CHOICES, null=True, blank=True)
