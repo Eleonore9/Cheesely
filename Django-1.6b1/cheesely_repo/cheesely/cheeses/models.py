7628561cf1c5cb4d6442c37e6eb66bab6921895e
Eleonore9 <eleonore.mayola@ymail.com>
2013-07-15 20:02:15 +0200

    added my favorite cheeses to my Cheeses app

--- Django-1.6b1/cheesely_repo/cheesely/cheeses/models.py
+++ Django-1.6b1/cheesely_repo/cheesely/cheeses/models.py
@@ -3,18 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Cheese(models.Model):
-	FIRMNESS_UNSPECIFIED = 1
-	FIRMNESS_SOFT = 2
-	FIRMNESS_HARD = 3
+    FIRMNESS_UNSPECIFIED = 1
+    FIRMNESS_SOFT = 2
+    FIRMNESS_HARD = 3
 
-	FIRMNESS_CHOICES = (
-		(FIRMNESS_UNSPECIFIED, 'unspecified'),
-		(FIRMNESS_SOFT, 'soft'),
-		(FIRMNESS_HARD, 'hard'),
-	)
+    FIRMNESS_CHOICES = (
+        (FIRMNESS_UNSPECIFIED, 'unspecified'),
+        (FIRMNESS_SOFT, 'soft'),
+        (FIRMNESS_HARD, 'hard'),
+    )
 
-	name = models.CharField("Name of Cheese", max_length=255)
-	description = models.TextField("Description", blank=True,
-				null=True)
-	firmness = models.IntegerField("Firmness", max_length=20,
-				choices=FIRMNESS_CHOICES, null=True, blank=True)
+    name = models.CharField("Name of Cheese", max_length=255)
+    description = models.TextField("Description", blank=True,
+                null=True)
+    firmness = models.IntegerField("Firmness", max_length=20,
+                choices=FIRMNESS_CHOICES, null=True, blank=True)
+
+    def __unicode__(self): 
+        # To display the actual name 
+        # and not "Cheese object"
+        return self.name
