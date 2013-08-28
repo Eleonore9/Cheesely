d7d51cd9a844591e5aad655e87fa76b71f3354bd
Eleonore9 <eleonore.mayola@ymail.com>
2013-08-28 12:43:07 +0200

    rm internationalization chges for now cause of bugs -Revert "Started internationalization of user model labels"
    This reverts commit 48f23720b51883d7a0ec062bccf421f5170d7b3e.

--- Django-1.6b1/cheesely_repo/cheesely/cheeses/models.py
+++ Django-1.6b1/cheesely_repo/cheesely/cheeses/models.py
@@ -1,34 +1,34 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
 
 class Cheese(models.Model):
-
     FIRMNESS_UNSPECIFIED = 1
     FIRMNESS_SOFT = 2
-    FIRMNESS_SEMISOFT = 3
-    FIRMNESS_SEMIHARD = 4
-    FIRMNESS_HARD = 5
+    FIRMNESS_HARD = 3
+
     FIRMNESS_CHOICES = (
-        (FIRMNESS_UNSPECIFIED, _("unspecified")),
-	(FIRMNESS_SOFT, _("soft")),
-	(FIRMNESS_SEMISOFT, _("semi-soft")),
-	(FIRMNESS_SEMIHARD, _("semi-hard")),
-	(FIRMNESS_HARD, _("hard")),
-	)
-    name = models.CharField(_("Name of Cheese", max_length=255))
-    description = models.TextField(_("Description", blank=True))
-    firmness = models.IntegerField(_("Firmness", max_length=20,
-		choices=FIRMNESS_CHOICES, null=True, blank=True))
-    country_of_origin = models.CharField(_("Country of Origin",
-                            max_length=255, blank=True, null=True))
-    region_of_origin = models.CharField(_("Region of Origin",
-                            max_length=255, blank=True, null=True))
-    photo = models.ImageField(_("Photo", upload_to="cheeses",
-                                        blank=True, null=True, default="")) 
-    photo_attribution = models.URLField(_("Photo Attribution",
-		    max_length=255, blank=True, null=True, default=""))
+        (FIRMNESS_UNSPECIFIED, 'unspecified'),
+        (FIRMNESS_SOFT, 'soft'),
+        (FIRMNESS_HARD, 'hard'),
+    )
+
+    name = models.CharField("Name of Cheese", max_length=255)
+    description = models.TextField("Description", blank=True,
+                null=True)
+    firmness = models.IntegerField("Firmness", max_length=20,
+                choices=FIRMNESS_CHOICES, null=True, blank=True)
+    country_of_origin = models.CharField("Country of Origin",
+                            max_length=255, blank=True, null=True)
+    region_of_origin = models.CharField("Region of Origin",
+                            max_length=255, blank=True, null=True)
+    photo = models.ImageField("Photo", upload_to="cheeses",
+                                        blank=True, null=True, default="") 
+    photo_attribution = models.URLField("Photo Attribution",
+		    max_length=255, blank=True, null=True, default="")
 
     def __unicode__(self): 
         # To display the actual name 
         # and not "Cheese object"
         return self.name
+
