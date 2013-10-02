cfdb18c059cd4da6f5e790c0375e95813a6d9d40
Eleonore9 <eleonore.mayola@ymail.com>
2013-10-02 19:43:46 +0200

    seems like i added a postgres db

--- Django-1.6b1/cheesely_repo/cheesely/cheesely/settings.py
+++ Django-1.6b1/cheesely_repo/cheesely/cheesely/settings.py
@@ -69,14 +69,14 @@ WSGI_APPLICATION = 'cheesely.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     },
     'postgresql': {
 	    'ENGINE': 'postgresql_psycopg2',
 	    'NAME': 'cheesely',
 	    'USER': 'eleonore',
 	    'HOST': '',
-	    'PORT': '',
+	    'PORT': ''
     }
 }	    
 
